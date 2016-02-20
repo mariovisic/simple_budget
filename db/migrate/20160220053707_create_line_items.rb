1017d4e22cb78ce6d38f2d321ddb12ccd069bf18
Mario Visic <mario@mariovisic.com>
2016-02-20 13:37:55 +0800

    Add line items

---
+++ db/migrate/20160220053707_create_line_items.rb
@@ -0,0 +1,12 @@
+class CreateLineItems < ActiveRecord::Migration
+  def change
+    create_table :line_items do |t|
+      t.string :name, null: false
+      t.string :company, null: false
+      t.decimal :amount, precision: 10, scale: 2, null: false
+      t.datetime :purchased_at, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220052802) do
+ActiveRecord::Schema.define(version: 20160220053707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20160220052802) do
     t.decimal  "weekly_deposit", precision: 10, scale: 2, null: false
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.string   "name",                                  null: false
+    t.string   "company",                               null: false
+    t.decimal  "amount",       precision: 10, scale: 2, null: false
+    t.datetime "purchased_at",                          null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
   end
 
 end
